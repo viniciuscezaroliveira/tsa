36d14ee476e9b4749e2e75b4bc5cee69d1e58ab0
Vinicius Oliveira <v.cezar21@gmail.com>
2020-08-14 20:03:19 -0300

    Check cpf e numero do cartao

--- assets/src/js/cadastro.js
+++ assets/src/js/cadastro.js
@@ -4,16 +4,17 @@ document.querySelector("#name").focus();
 const showError = (element) => {
     element.classList.remove("form__input")
     element.classList.add("form__input__error")
-    document.getElementById(`errorinput${element.name}`).classList.remove("form__input__error_none__display")
-    document.getElementById(`errorinput${element.name}`).classList.add("form__input__error__display")
+    document.querySelector(`#errorinput${element.name} label`).classList.remove("form__input__error_none__display")
+    document.querySelector(`#errorinput${element.name} label`).classList.add("form__input__error__display")
     element.focus()
 }
 
 const hideError = (element) => {
+
     element.classList.remove("form__input__error");
     element.classList.add("form__input");
-    document.getElementById(`errorinput${element.name}`).classList.remove("form__input__error__display")
-    document.getElementById(`errorinput${element.name}`).classList.add("form__input__error_none__display")
+    document.querySelector(`#errorinput${element.name} label`).classList.remove("form__input__error__display")
+    document.querySelector(`#errorinput${element.name} label`).classList.add("form__input__error_none__display")
 }
 //END REGION
 
@@ -125,17 +126,142 @@ const regExpNumberReplace = (strNumber) => {
     return strNumber
 }
 
+const validCep = (val) => {
+    // Caso o CEP não esteja nesse formato ele é inválido!
+    var objER = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/;
+    let cep = ""
+    cep = val.value.replace(/^s+|s+$/g, '');
+    if (cep.length > 0) {
+        if (objER.test(cep)) {
+            hideError(val)
+            return true;
+        }
+        else {
+            showError(val)
+            return false
+        }
+
+    }
+
+}
+
+
+const checkCard = (val) => {
+    if (val.value.length == 19) {
+        var msg = Array();
+        var tipo = null;
+        var cardNumber = val.value.replace(/\s/g, "")
+        if (cardNumber.length > 16 || cardNumber[0] == 0) {
+            showError(val)
+            document.querySelector(`#errorinput${val.name} label`).innerHTML = "Cartão inválido"
+
+        } else {
+
+            var total = 0;
+            var arr = Array();
+            var operadora = ""
+            for (i = 0; i < cardNumber.length; i++) {
+                if (i % 2 == 0) {
+                    dig = cardNumber[i] * 2;
+
+                    if (dig > 9) {
+                        dig1 = dig.toString().substr(0, 1);
+                        dig2 = dig.toString().substr(1, 1);
+                        arr[i] = parseInt(dig1) + parseInt(dig2);
+                    } else {
+                        arr[i] = parseInt(dig);
+                    }
+
+                    total += parseInt(arr[i]);
+
+                } else {
+
+                    arr[i] = parseInt(cardNumber[i]);
+                    total += parseInt(arr[i]);
+                }
+            }
+
+            switch (parseInt(cardNumber[0])) {
+                case 0:
+                    document.querySelector(`#errorinput${val.name} label`).innerHTML = "Cartão inválido"
+                    break;
+                case 1:
+                    tipo = "Empresas Aéreas";
+                    break;
+                case 2:
+                    tipo = "Empresas Aéreas";
+                    break
+                case 3:
+                    tipo = "Viagens e Entretenimento";
+                    if (parseInt(cardNumber[0] + cardNumber[1]) == 34 || parseInt(cardNumber[0] + cardNumber[1]) == 37) { operadora = "Amex"; }
+                    if (parseInt(cardNumber[0] + cardNumber[1]) == 36) { operadora = "Diners"; }
+                    break
+                case 4:
+                    tipo = "Bancos e Instituições Financeiras";
+                    operadora = "visa";
+                    break
+                case 5:
+                    if (parseInt(cardNumber[0] + cardNumber[1]) >= 51 && parseInt(cardNumber[0] + cardNumber[1]) <= 55) { operadora = "Mastercard"; }
+                    tipo = "Bancos e Instituições Financeiras";
+                    operadora = "Mastercard"
+                    break;
+                case 6:
+                    tipo = "Bancos e Comerciais";
+                    operadora = "";
+                    break
+                case 7:
+                    tipo = "Companhias de petróleo";
+                    operadora = "";
+                    break
+                case 8:
+                    tipo = "Companhia de telecomunicações";
+                    operadora = "";
+                    break
+                case 9:
+                    tipo = "Nacionais";
+                    operadora = "";
+                    break
+                default:
+                    document.querySelector(`#errorinput${val.name} label`).innerHTML = "Cartão inválido"
+                    break;
+            }
+
+        }
+
+        if (msg.length > 0) {
+
+            console.log(msg);
+
+        } else {
+
+            console.log(cardNumber);
+            showError(val)
+            if (total % 10 == 0) {
+                hideError(val)
+                // console.log("Cartão válido: (" + total + ")");
+                // console.log("Tipo: " + tipo);
+                // console.log("Operadora: " + operadora);
+                return true
+            } else {
+                document.querySelector(`#errorinput${val.name} label`).innerHTML = "Cartão inválido"
+                return false
+            }
+        }
+    }
+
+}
+
 const validInputNumber = (val, cpf = false) => {
-    let regExp = new RegExp(/^[0-9.,-]+$/)
+    let regExp = new RegExp(/^[0-9]+$/)
 
 
     if (!regExp.test(val.value) && cpf == false) {
-        console.log(val.value)
         showError(val)
-        document.getElementById(`errorinput${val.name}`).innerHTML = "Apenas números são permitidos"
+        document.querySelector(`#errorinput${val.name} label`).innerHTML = "Apenas números são permitidos"
         return false
     }
     else if (regExp.test(val.value) && cpf == false) {
+        console.log(val)
         hideError(val)
         return true
     }
@@ -144,14 +270,14 @@ const validInputNumber = (val, cpf = false) => {
         if (cpf == true && regExpCpfReplace(val.value).length != 11) {
             showError(val)
             let cpfLength = (11 - (regExpCpfReplace(val.value)).length)
-            document.getElementById(`errorinput${val.name}`).innerHTML = `O CPF deve ter 11 digitos : ${regExpCpfReplace(val.value).length <= 11 ? `Falta ${cpfLength}` : `Sobra ${cpfLength * -1}`}  caracters`
+            document.querySelector(`#errorinput${val.name} label`).innerHTML = `O CPF deve ter 11 digitos : ${regExpCpfReplace(val.value).length <= 11 ? `Falta ${cpfLength}` : `Sobra ${cpfLength * -1}`}  caracters`
             return false;
         }
 
     }
     if (cpf == true && (regExpCpfReplace(val.value)).length == 11 && !cpfValid(regExpCpfReplace(val.value))) {
         showError(val)
-        document.getElementById(`errorinput${val.name}`).innerHTML = "CPF é invalido, verifique!"
+        document.querySelector(`#errorinput${val.name} label`).innerHTML = "CPF é invalido, verifique!"
         return false;
     }
     else if (cpf == true && (regExpCpfReplace(val.value)).length == 11 && cpfValid(regExpCpfReplace(val.value))) {
@@ -266,7 +392,7 @@ const loadCity = (uf) => {
 }
 
 const loadMonthDataCard = () => {
-    let now = new Date
+
     let selectObjMonth = document.querySelector("#monthDataCard")
     let selectObjYear = document.querySelector("#yearDataCard")
 
@@ -315,35 +441,43 @@ const frmValid = (frm) => {
         validInputWord(frm.name) &&
         validInputEmail(frm.email) &&
         validInputNumber(frm.cpf, true) &&
+        validCep(frm.cep) &&
         frm.name.value != "" &&
         frm.email.value != "" &&
         frm.cpf.value != ""
 
     ) {
 
-        alert("O cadastro foi realizado com sucesso!")
-        let frmJson = []
-        console.log(frm.name.value)
-        console.log(frm.email.value)
-        console.log(frm.cpf.value)
-        console.log(frm.address.value)
-        console.log(frm.uf.value)
-        console.log(frm.city.value)
-        console.log(frm.cep.value)
-        console.log(frm.payament_form.value)
 
+        let frmJson = []
+        let now = new Date
+        let dateNow = `${now.getDate()}-${now.getMonth() + 1}-${now.getFullYear()}`
         let payament_form = []
+
         if (frm.payament_form.value == 'credit') {
-            payament_form = {
-                'credit': {
-                    'card_number': frm.card__name.value,
-                    'expiration_date_month': frm.monthDataCard.value,
-                    'expiration_date_year': frm.yearDataCard.value,
-                    'vard_number': frm.card__number.value,
-                    'security_code': frm.security_code.value
+
+            if (checkCard(frm.card__number) &&
+                validInputWord(frm.card__name) &&
+                validInputNumber(frm.security_code)) {
+                payament_form = {
+                    'credit': {
+                        'card_number': frm.card__name.value,
+                        'expiration_date_month': frm.monthDataCard.value,
+                        'expiration_date_year': frm.yearDataCard.value,
+                        'vard_number': frm.card__number.value,
+                        'security_code': frm.security_code.value
+                    }
                 }
+
             }
+            else
+                return false
+
+
         }
+        else if (frm.payament_form.value == 'boleto')
+            payament_form = frm.payament_form.value
+
         frmJson = {
             "name": frm.name.value,
             "email": frm.email.value,
@@ -352,8 +486,10 @@ const frmValid = (frm) => {
             "uf": frm.uf.value,
             "street": frm.city.value,
             "cep": frm.cep.value,
-            "payament_form": (frm.payament_form.value == 'credit' ? payament_form : frm.payament_form.value)
+            "payament_form": payament_form,
+            "create": dateNow
         }
+        console.log(frmJson)
 
         if (localStorage.getItem(`register`) != null && localStorage.getItem(`register`) != undefined) {
             let registers = JSON.parse(localStorage.getItem(`register`))
@@ -366,7 +502,9 @@ const frmValid = (frm) => {
             localStorage.setItem(`register`, JSON.stringify(registers))
         }
 
+        //alert("O cadastro foi realizado com sucesso!")
         return false
+        //document.location.reload(true)
 
     }
     else return false
--- assets/src/js/lista.js
+++ assets/src/js/lista.js
@@ -1,8 +1,75 @@
 document.getElementById("search_client").focus();
 
+const loadTable = () => {
+    removeCellTable()
+    let inputSearch = document.querySelector("#search_client").value
+    if (inputSearch == null || inputSearch == "") {
+        for (let i = 0; i < JSON.parse(localStorage.getItem('register')).length; i++) {
+            const element = JSON.parse(localStorage.getItem('register'))[i];
+            insetCellTable(element)
 
-for (let i = 0; i < JSON.parse(localStorage.getItem('register')).length; i++) {
-    const element = JSON.parse(localStorage.getItem('register'))[i];
-    console.log(element['name'])
+        }
+    }
+    else {
+
+        for (let i = 0; i < JSON.parse(localStorage.getItem('register')).length; i++) {
+            const element = JSON.parse(localStorage.getItem('register'))[i];
+
+            if (
+                (element["name"].toLowerCase()).search(inputSearch.toLowerCase()) != -1 ||
+                (element["email"].toLowerCase()).search(inputSearch.toLowerCase()) != -1 ||
+                (element["cpf"].toLowerCase()).search(inputSearch.toLowerCase()) != -1
+
+
+            ) {
+                //console.log((element["name"].toLowerCase()).replace(/\s/g, ''))
+                //console.log(element["email"].toLowerCase().search(inputSearch.toLowerCase()))
+                insetCellTable(element)
+            }
+
+        }
+
+
+    }
+
 
 }
+
+
+
+const insetCellTable = (element) => {
+
+    let tableRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+    let newRow = tableRef.insertRow(tableRef.rows.length);
+    let newCell = newRow.insertCell(0);
+    let newCell1 = newRow.insertCell(1);
+    let newCell2 = newRow.insertCell(2);
+    let newCell3 = newRow.insertCell(3);
+
+    // Append a text node to the cell
+    let name = document.createTextNode(element["name"]);
+    let email = document.createTextNode(element["email"]);
+    let cpf = document.createTextNode(element["cpf"]);
+    let create = document.createTextNode(element["create"]);
+    newCell.appendChild(name);
+    newCell1.appendChild(email);
+    newCell2.appendChild(cpf);
+    newCell3.appendChild(create);
+}
+
+const removeCellTable = () => {
+    let tableRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+    let length = tableRef.rows.length
+
+    if (tableRef.rows.length > 0) {
+        for (let i = 0; i < length; i++) {
+            tableRef.deleteRow(0)
+        }
+    }
+
+}
+
+
+
+
+loadTable()
